102d5b951edd1444eff5fafd406f2d5be277a39d
ImDarkTom <darktomg21@gmail.com>
2023-06-09 17:56:15 +0100

    ✨ additional generation options

--- src/commands/imageCreation/generate.js
+++ src/commands/imageCreation/generate.js
@@ -15,6 +15,20 @@ module.exports = {
             description: 'What to generate.',
             required: true,
             type: ApplicationCommandOptionType.String,
+        },
+        {
+            name: 'steps',
+            description: "How much time to spend generating.",
+            required: false,
+            type: ApplicationCommandOptionType.Integer,
+            choices: sdConfig.steps_choices
+        },
+        {
+            name: 'cfg',
+            description: "How much creative freedom when generating.",
+            required: false,
+            type: ApplicationCommandOptionType.Integer,
+            choices: sdConfig.cfg_choices
         }
     ],
 
@@ -24,7 +38,8 @@ module.exports = {
         const imageData = await sendRequest('txt2img', {
             prompt: interaction.options.get('prompt').value,
             negative_prompt: sdConfig.defaultNegativePrompt,
-            steps: 20
+            steps: interaction.options.get('steps')?.value || 20,
+            cfg_scale: interaction.options.get('cfg')?.value || 7
         });
 
         await interaction.editReply(await createImageEmbed(imageData, {
