03a1b0732c13329f0db8b95179a2a7c88f1c35f1
ImDarkTom <darktomg21@gmail.com>
2023-06-10 15:22:11 +0100

    ⚡️ imageDataFromEmbed optimisation

--- src/utils/SD/imageDataFromEmbed.js
+++ src/utils/SD/imageDataFromEmbed.js
@@ -3,27 +3,23 @@ const axios = require("axios");
 module.exports = async(embed, returnImageB64 = false) => {
     const embedFields = embed.fields;
 
-    if (returnImageB64) {
-        const response = await axios.get(embed.image.url, {responseType: 'arraybuffer'});
-        const base64 = Buffer.from(response.data, 'binary').toString('base64');
-
-        return {
-            image: base64,
-            width: embed.image.width,
-            height: embed.image.height,
-            prompt: embed.title.match(/"([^"]*)"/)[1],
-            cfg_scale: embedFields[2].value,
-            steps: embedFields[1].value,
-            seed: embedFields[0].value,
-            model: embed.footer.text
-        }
-    }
-
-    return {
+    const data = {
         prompt: embed.title.match(/"([^"]*)"/)[1],
         cfg_scale: embedFields[2].value,
         steps: embedFields[1].value,
         seed: embedFields[0].value,
         model: embed.footer.text
+    };
+
+    if (returnImageB64) {
+        const embedImage = embed.image;
+        const response = await axios.get(embedImage.url, {responseType: 'arraybuffer'});
+        const base64 = Buffer.from(response.data, 'binary').toString('base64');
+
+        data.image = base64;
+        data.width = embedImage.width;
+        data.height = embedImage.height;
     }
+
+    return data;
 }
