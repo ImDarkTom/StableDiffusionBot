7fc0159c6bfc6c11cd1770be17411877f8e33bca
ImDarkTom <darktomg21@gmail.com>
2023-06-10 15:27:35 +0100

    ⚡️ createImageEmbed optimisation

--- src/utils/SD/createImageEmbed.js
+++ src/utils/SD/createImageEmbed.js
@@ -6,57 +6,31 @@ module.exports = async (data, settings = { saveBtn: true, upscaleBtn: true, redo
 
     const imageAttachment = new AttachmentBuilder(await base64ToBuffer(data.images[0]), { name: 'output.png'});
     const imageParams = JSON.parse(data.info); //imageData.parameters doesn't contain info such as seed or sampler_name.
-    let embed;
 
-    if (botConfig.showImageAuthor) {
-        embed = new EmbedBuilder()
-            .setAuthor({name: `${user.username}#${user.discriminator}`, iconURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`})
-            .addFields([
-                {
-                    name: "Seed",
-                    value: imageParams.seed.toString(),
-                    inline: true
-                },
-                {
-                    name: "Steps",
-                    value: imageParams.steps.toString(),
-                    inline: true
-                },
-                {
-                    name: "CFG SCale",
-                    value: imageParams.cfg_scale.toString(),
-                    inline: true
-                }
-            ])
-            .setTitle(`${context != "" ? `${context} - ` : ""}"${imageParams.prompt}"`)
-            .setImage('attachment://output.png')
-            .setFooter({text: imageParams.infotexts[0].match(/Model: ([^,]+)/)[1]})
-            .setColor("#00bb00")
-    } else {
-        embed = new EmbedBuilder()
-            .addFields([
-                {
-                    name: "Seed",
-                    value: imageParams.seed.toString(),
-                    inline: true
-                },
-                {
-                    name: "Steps",
-                    value: imageParams.steps.toString(),
-                    inline: true
-                },
-                {
-                    name: "CFG SCale",
-                    value: imageParams.cfg_scale.toString(),
-                    inline: true
-                }
-            ])
-            .setTitle(`${context != "" ? `${context} - ` : ""}"${imageParams.prompt}"`)
-            .setImage('attachment://output.png')
-            .setFooter({text: imageParams.infotexts[0].match(/Model: ([^,]+)/)[1]})
-            .setColor("#00bb00")
-    }
-    
+    let embed = new EmbedBuilder()
+        .addFields([
+            {
+                name: "Seed",
+                value: imageParams.seed.toString(),
+                inline: true
+            },
+            {
+                name: "Steps",
+                value: imageParams.steps.toString(),
+                inline: true
+            },
+            {
+                name: "CFG SCale",
+                value: imageParams.cfg_scale.toString(),
+                inline: true
+            }
+        ])
+        .setTitle(`${context != "" ? `${context} - ` : ""}"${imageParams.prompt}"`)
+        .setImage('attachment://output.png')
+        .setFooter({ text: imageParams.infotexts[0].match(/Model: ([^,]+)/)[1] })
+        .setColor("#00bb00")
+
+    if (botConfig.showImageAuthor) { embed.setAuthor({name: `${user.username}#${user.discriminator}`, iconURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}?size=256`}); }
 
     const row = new ActionRowBuilder()
     
