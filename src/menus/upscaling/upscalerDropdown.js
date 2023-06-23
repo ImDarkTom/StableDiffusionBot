84e28bbbcb60d7dd2d306bcd6f38a9730b5d93a5
ImDarkTom <darktomg21@gmail.com>
2023-06-23 16:44:27 +0100

    💄 upscaler menu embed shows name

--- src/menus/upscaling/upscalerDropdown.js
+++ src/menus/upscaling/upscalerDropdown.js
@@ -1,12 +1,23 @@
 const { EmbedBuilder } = require("discord.js");
+const sdConfig = require('../../../sdConfig.json');
 
 module.exports = {
     id: 'upscalerDropdown',
     ownerOnly: true,
 
     callback: async (client, interaction) => {
+        const upscalerIndex = interaction.values[0];
+        let upscalerName;
+
+        for (const upscaler of sdConfig.upscalers) {
+            if (String(upscaler.value) === upscalerIndex) {
+                upscalerName = upscaler.name;
+            }
+        }
+
         const embed = new EmbedBuilder()
-            .setTitle(`Selected '${interaction.values[0]}'`)
+            .setTitle(`Selected '${upscalerName}' upscaler.`)
+            .setColor(0x0080b0)
 
         await interaction.update({content: `${interaction.message.content.split("-")[0]}-${interaction.values[0]}`, components: [interaction.message.components[1]], embeds: [embed]});
     },
